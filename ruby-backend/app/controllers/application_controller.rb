87e1cf8c879667dd1d6d65618f969302db8b0549
Job Muturi <muirimuturi17@gmail.com>
2023-06-08 00:16:12 +0300

    Configured backend routes

--- ruby-backend/app/controllers/application_controller.rb
+++ ruby-backend/app/controllers/application_controller.rb
@@ -5,5 +5,78 @@ class ApplicationController < Sinatra::Base
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
+  get '/exercises' do
+    exercises = Exercise.all
+    exercises.to_json(include: [:user])
+  end
+
+  get '/exercises/:id' do
+    exercise = Exercise.find(params[:id])
+    exercise.to_json(include: [:user])
+  end
+
+  post '/exercises' do
+    exercise = Exercise.create(
+      name: params[:name],
+      muscle_group: params[:muscle_group],
+      weight: params[:weight],
+      sets: params[:sets],
+      reps: params[:reps],
+      rest: params[:rest],
+      user_id: params[:user_id]
+    )
+    exercise.to_json(include: [:user])
+  end
+
+  # build DELETE route - DELETE specific route, (.find, .destroy)
+  delete '/exercises/:id' do
+    exercise = Exercise.find(params[:id])
+    exercise.destroy
+    exercise.to_json
+  end
+
+  patch '/exercises/:id' do
+    exercise = Exercise.find(params[:id])
+    
+    exercise.update(
+      name: params[:name],
+      muscle_group: params[:muscle_group],
+      weight: params[:weight],
+      sets: params[:sets],
+      reps: params[:reps],
+      rest: params[:rest],
+      user_id: params[:user_id]
+    )
+    exercise.to_json(include: [:user])
+    
+  end
+#Routes for the users
+get '/users' do
+  users = User.all
+  users.to_json(include: [:exercises])
+end
+
+get '/users/:id' do
+  user = User.find(params[:id])
+  user.to_json(include: [:exercises])
+end
+
+post '/users' do
+  user = User.create(params)
+  user.to_json(include: [:exercises])
+end
+
+delete '/users/:id' do
+  user = User.find(params[:id])
+  user.destroy
+  user.to_json
+end
+
+patch '/users/:id' do
+  user = User.find(params[:id])
+  user.update(
+    name: params[:name]
+  )
+end
 
 end
